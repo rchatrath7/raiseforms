9ef059e1d48f21027383af5d435cbeba7e4a2bb0
Rakesh Chatrath <rchatrath7@gmail.com>
2016-07-28 17:40:20 -0500

    Initial models. Need to add validators, and properties.

--- forms/models.py
+++ forms/models.py
@@ -5,7 +5,8 @@ from django.db import models
 
 
 class Executive(models.Model):
-    # Raise executive. We need to get the name and email of the Raise executive for use when signing the docusign document.
+    # Raise executive. We need to get the name and email of the Raise executive for use when signing the docusign
+    # document.
     # TODO: Adding more fields and any necessary methods. Make a more secure way of hasing and storing passwords.
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=30)
@@ -15,10 +16,10 @@ class Executive(models.Model):
     password = models.CharField(max_length=16)
 
     # Relations
-    client = models.ForeignKey(Client)
+    # client = models.ForeignKey('Client')
 
     def __str__(self):
-       return "<Name: " + self.name +" Email: " + self.email + ">"
+        return "<Name: " + self.name +" Email: " + self.email + ">"
 
 
 class Client(models.Model):
@@ -26,19 +27,21 @@ class Client(models.Model):
     # All necessary information at once.
     # TODO: Figure out file uploading/googledocs support.
     def file_upload_path(self, instance, filename):
-        '''Upload file to MEDIA_ROOT/user.id/filename'''
+        # Upload file to MEDIA_ROOT/user.id/filename
         # Perhaps adjust for renaming the files from something less ugly to something more readable - maybe the file ID
         # We could also remove all file handling to the individual classes
         return str(self.id)+'/'+filename
 
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=30)
+    address = models.CharField(max_length=100)
     nda = models.FileField(upload_to=file_upload_path)
     statement_of_work = models.FileField(upload_to=file_upload_path)
+    consulting_agreement = models.FileField(upload_to=file_upload_path)
     purchase_request = models.FileField(upload_to=file_upload_path)
 
     # Relations
-    executive = models.ForeignKey(Executive)
+    # executive = models.ForeignKey(Executive)
 
     def __str__(self):
         return "<Name: " + self.name + " Email: " + self.email + ">"
@@ -47,15 +50,15 @@ class Client(models.Model):
 class NDA(models.Model):
     # TODO: Put in validators
     # NDA FORM
-    ssn = models.CharField(max_length=9, validator=[])
+    ssn = models.CharField(max_length=9)
     location = models.CharField(max_length=100)
     corporation = models.CharField(max_length=100)
     title = models.CharField(max_length=20)
     agreement_date = models.DateTimeField(auto_now_add=True)
 
     # Relations
-    client = models.ForeignKey(Client)
-    executive = models.ForeignKey(Executive)
+    # client = models.ForeignKey(Client)
+    # executive = models.ForeignKey(Executive)
 
 
 class StatementOfWork(models.Model):
@@ -68,13 +71,47 @@ class StatementOfWork(models.Model):
     # Milestone tables
     deliverables = models.BooleanField()
     # Deliverables Table
-    # We can probably use a much better way of determining which payment method is used, but for now
+    # We can probably use a much better way of determining which payment method is used, but for now will just have fields
+    hourly_pricing = models.BooleanField()
+    fixed_pricing = models.BooleanField()
+    milestone_pricing = models.BooleanField()
+    hourly_rate = models.FloatField()
+    fixed_rate = models.FloatField()
+    milestone_rate = models.FloatField()
+    additional_terms_of_services = models.TextField()
 
 
+class ConsultingAgreement(models.Model):
+    agreement_date = models.DateTimeField(auto_now_add=True)
 
 
+class PurchaseRequest(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    cost_center = models.CharField(max_length=50)
+    # Client/Owner
+    requester = models.ForeignKey(Client)
+    owner = models.ForeignKey(Executive)
+    vendor = models.CharField()
+    description = models.TextField()
+    purpose = models.TextField()
+    amount = models.FloatField()
+    # This should be a date range, just adjust it to get the difference in time
+    service_period = models.DateTimeField()
+    # Handle 'either-ors' better (invoice, payment_type, etc)
+    contract_present = models.BooleanField(default=False)
+    invoice_cadence_recurring = models.BooleanField(default=False)
+    if invoice_cadence_recurring:
+        recurring_date = models.DateTimeField()
+    payment_types = {
+        'ach': models.BooleanField(default=False),
+        'credit': models.BooleanField(default=False),
+        'check': models.BooleanField(default=False),
+        'abacus': models.BooleanField(default=False)
+    }
+    payment_terms_net_30 = models.BooleanField(default=False)
+    payment_terms_receipt = models.BooleanField(default=False)
 
-
+    additional_notes = models.TextField()
 
 
 
