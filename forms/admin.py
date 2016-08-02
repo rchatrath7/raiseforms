17ae3fce4b113487f119f6cb6434a0599470f572
Rakesh Chatrath <rchatrath7@gmail.com>
2016-08-01 22:55:29 -0500

    Add .gitignore and Update the models. Register models in admin module for initial testing

--- forms/admin.py
+++ forms/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import Executive, Client, NDA, StatementOfWork, ConsultingAgreement, PurchaseRequest
 
 # Register your models here.
+admin.site.register(Executive)
+admin.site.register(Client)
+admin.site.register(NDA)
+admin.site.register(StatementOfWork)
+admin.site.register(ConsultingAgreement)
+admin.site.register(PurchaseRequest)
--- forms/models.py
+++ forms/models.py
@@ -4,6 +4,13 @@ from django.db import models
 # Create your models here.
 
 
+def file_upload_path(instance, filename):
+    # Upload file to MEDIA_ROOT/user.id/filename
+    # Perhaps adjust for renaming the files from something less ugly to something more readable - maybe the file ID
+    # We could also remove all file handling to the individual classes
+    return str(self.id) + '/' + filename
+
+
 class Executive(models.Model):
     # Raise executive. We need to get the name and email of the Raise executive for use when signing the docusign
     # document.
@@ -13,38 +20,29 @@ class Executive(models.Model):
     username = models.CharField(max_length=30)
     title = models.CharField(max_length=30)
     # We need to use a better way of storing the password - Use hashing algorithm and some sort of setter
-    password = models.CharField(max_length=16)
-
-    # Relations
-    # client = models.ForeignKey('Client')
+    # password = models.CharField(max_length=16)
 
     def __str__(self):
-        return "<Name: " + self.name +" Email: " + self.email + ">"
+        return "<Name: %s, Email: %s>" % (self.name, self.email)
 
 
 class Client(models.Model):
     # The client is the person signing. We want to be able to send them multiple forms if possible, as well as gather
     # All necessary information at once.
     # TODO: Figure out file uploading/googledocs support.
-    def file_upload_path(self, instance, filename):
-        # Upload file to MEDIA_ROOT/user.id/filename
-        # Perhaps adjust for renaming the files from something less ugly to something more readable - maybe the file ID
-        # We could also remove all file handling to the individual classes
-        return str(self.id)+'/'+filename
-
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=30)
     address = models.CharField(max_length=100)
-    nda = models.FileField(upload_to=file_upload_path)
+    nda_file = models.FileField(upload_to=file_upload_path)
     statement_of_work = models.FileField(upload_to=file_upload_path)
     consulting_agreement = models.FileField(upload_to=file_upload_path)
     purchase_request = models.FileField(upload_to=file_upload_path)
 
     # Relations
-    # executive = models.ForeignKey(Executive)
+    executive = models.ForeignKey(Executive)
 
     def __str__(self):
-        return "<Name: " + self.name + " Email: " + self.email + ">"
+        return "<Name: %s, Email: %s>" % (self.name, self.email)
 
 
 class NDA(models.Model):
@@ -57,8 +55,13 @@ class NDA(models.Model):
     agreement_date = models.DateTimeField(auto_now_add=True)
 
     # Relations
-    # client = models.ForeignKey(Client)
-    # executive = models.ForeignKey(Executive)
+    client = models.ForeignKey(Client)
+    executive = models.ForeignKey(Executive)
+
+    def __str__(self):
+        return "<SSN: %s, Location: %s, Corporation: %s, Title: %s, Agreement Date: %s, Client: %s, Executive: %s>"\
+            % (self.ssn, self.location, self.corporation, self.title, self.agreement_date, self.client, self.executive)
+
 
 
 class StatementOfWork(models.Model):
@@ -80,18 +83,34 @@ class StatementOfWork(models.Model):
     milestone_rate = models.FloatField()
     additional_terms_of_services = models.TextField()
 
+    # Relations
+    client = models.ForeignKey(Client)
+    executive = models.ForeignKey(Executive)
+
+    def __str__(self):
+        return "<Statement Of Work: %s, Client: %s, Executive: %s>" % (self.id, self.client, self.executive)
+
 
 class ConsultingAgreement(models.Model):
     agreement_date = models.DateTimeField(auto_now_add=True)
 
+    # Relations
+    client = models.ForeignKey(Client)
+    executive = models.ForeignKey(Executive)
+
+    def __str__(self):
+        return "<Consulting Agreement: %s, Agreement Date: %s, Client: %s, Executive: %s>"\
+               % (self.id, self.agreement_date, self.client, self.executive)
+
 
 class PurchaseRequest(models.Model):
+    # TODO: I'd like to add properties and in general cleaner ways of handling things
     date = models.DateTimeField(auto_now_add=True)
     cost_center = models.CharField(max_length=50)
     # Client/Owner
     requester = models.ForeignKey(Client)
     owner = models.ForeignKey(Executive)
-    vendor = models.CharField()
+    vendor = models.CharField(max_length=30)
     description = models.TextField()
     purpose = models.TextField()
     amount = models.FloatField()
@@ -100,8 +119,14 @@ class PurchaseRequest(models.Model):
     # Handle 'either-ors' better (invoice, payment_type, etc)
     contract_present = models.BooleanField(default=False)
     invoice_cadence_recurring = models.BooleanField(default=False)
-    if invoice_cadence_recurring:
-        recurring_date = models.DateTimeField()
+    recurring_date = models.DateTimeField()
+    choices = (
+        ('ach', 'ACH'),
+        ('credit', 'Credit'),
+        ('check', 'Check'),
+        ('abacus', 'Abacus')
+    )
+    payment_type = models.CharField(choices=choices, max_length=6)
     payment_types = {
         'ach': models.BooleanField(default=False),
         'credit': models.BooleanField(default=False),
@@ -113,5 +138,6 @@ class PurchaseRequest(models.Model):
 
     additional_notes = models.TextField()
 
-
-
+    def __str__(self):
+        return "<Purchase Request: %s, Date: %s, Cost Center: %s, Requester: %s, Owner: %s, Payment Type: %s>"\
+            % (self.id, self.date, self.cost_center, self.requester, self.owner, self.payment_type)
