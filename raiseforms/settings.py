da26f386daf3d03fa25e51ce6ca345ddf357784a
Rakesh Chatrath <rchatrath7@gmail.com>
2016-08-06 21:22:41 -0500

    Add styuling and sass support.

--- raiseforms/settings.py
+++ raiseforms/settings.py
@@ -121,7 +121,13 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
