ffdaed35f6d56bbbba900488fc9ac8054f41cd7e
Rakesh Chatrath <rchatrath7@gmail.com>
2016-08-28 15:27:57 -0400

    Fix ImportErorr with celery.schedules.

--- raiseforms/settings.py
+++ raiseforms/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
+from __future__ import absolute_import
 import os
 import sys
 import dj_database_url
