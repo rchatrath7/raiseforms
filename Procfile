2bd63297c17b8768a4d8348c4ca80598b8a7c736
Rakesh Chatrath <rchatrath7@gmail.com>
2016-08-28 20:19:00 -0400

    Prepapre for Heroku celery beat support.

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn raiseforms.wsgi --log-file -
+worker: python manage.py celery worker -B -l info
--- forms/utils.py
+++ forms/utils.py
@@ -1,13 +1,18 @@
 from django.core.files import File
 from hellosign_sdk import HSClient as HS
+from celery.utils.log import get_task_logger
 import tempfile
 
 
+logger = get_task_logger(__name__)
+
 def download_documents(api_key, model):
+    logger.info("Starting retrieval")
     hsClient = HS(api_key=api_key)
     clients = [client.client if client.is_active else None for client in model.objects.filter(
         account_type='C'
     )]
+    logger.info("Got clients: <%s>" % clients)
     document_types = ['nda', 'statement_of_work', 'consulting_agreement']
     for client in clients:
         if client and client.active_request_id:
@@ -15,6 +20,7 @@ def download_documents(api_key, model):
                 if getattr(client, '{}_status'.format(_type)) == 'pending':
                     document_type = _type
             request = hsClient.get_signature_request(client.active_request_id)
+            logger.info("Gathered signature request: %s" % request)
             if request.is_complete:
                 document = tempfile.TemporaryFile()
                 status = client.get_signature_request_file(
@@ -23,10 +29,14 @@ def download_documents(api_key, model):
                     file_type='pdf'
                 )
                 if status:
+                    logger.info("Successfully downloaded document: %s" % document)
                     doc_file = getattr(client, '{}_file'.format(document_type))
                     doc_file.save('{}/{}/{}'.format(client.user.get_full_name(), document_type,
                                                     getattr(client, '{}_id'.format(document_type))),
                                   File(document), save=True)
                     client.active_request_id = None
                     client.save()
+                else:
+                    logger.error("Error downloading document: <%s, %s>" % (document, status))
                 document.close()
+                logger.info("Closed document.")
