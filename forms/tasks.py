fac694e28c6d142d99434110fbeb75de342e7fb5
Rakesh Chatrath <rchatrath7@gmail.com>
2016-08-28 21:14:49 -0400

    Add task decorator

--- forms/tasks.py
+++ forms/tasks.py
@@ -1,11 +1,12 @@
 
 from celery.utils.log import get_task_logger
-
+from celery.task import task
 from utils import download_documents
 
 logger = get_task_logger(__name__)
 
 
+@task
 def task_download_documents():
     download_documents()
     logger.info("Downloaded all available documents")
