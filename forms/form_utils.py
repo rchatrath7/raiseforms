7875aca10c7dd25afe3260d8bb74db90d5638764
Rakesh Chatrath <rchatrath7@gmail.com>
2016-09-03 01:43:53 -0400

    Fix reference error

--- forms/form_utils.py
+++ forms/form_utils.py
@@ -17,29 +17,31 @@ def download_documents(api_key):
     )]
     logger.info("Got clients: <%s>" % clients)
     document_types = ['nda', 'statement_of_work', 'consulting_agreement']
+    document_type = None
     for client in clients:
         if client and client.active_request_id:
             for _type in document_types:
                 if getattr(client, '{}_status'.format(_type)) == 'pending':
                     document_type = _type
-            request = hsClient.get_signature_request(client.active_request_id)
-            logger.info("Gathered signature request: %s" % request)
-            if request.is_complete:
-                document = tempfile.TemporaryFile()
-                status = hsClient.get_signature_request_file(
-                    signature_request_id=str(client.active_request_id),
-                    path_or_file=document,
-                    file_type='pdf'
-                )
-                if status:
-                    logger.info("Successfully downloaded document: %s" % document)
-                    doc_file = getattr(client, '{}_file'.format(document_type))
-                    doc_file.save('{}/{}/{}'.format(client.user.get_full_name(), document_type,
-                                                    getattr(client, '{}_id'.format(document_type))),
-                                  File(document), save=True)
-                    client.active_request_id = None
-                    client.save()
-                else:
-                    logger.error("Error downloading document: <%s, %s>" % (document, status))
-                document.close()
-                logger.info("Closed document.")
+            if document_type:
+                request = hsClient.get_signature_request(client.active_request_id)
+                logger.info("Gathered signature request: %s" % request)
+                if request.is_complete:
+                    document = tempfile.TemporaryFile()
+                    status = hsClient.get_signature_request_file(
+                        signature_request_id=str(client.active_request_id),
+                        path_or_file=document,
+                        file_type='pdf'
+                    )
+                    if status:
+                        logger.info("Successfully downloaded document: %s" % document)
+                        doc_file = getattr(client, '{}_file'.format(document_type))
+                        doc_file.save('{}/{}/{}'.format(client.user.get_full_name(), document_type,
+                                                        getattr(client, '{}_id'.format(document_type))),
+                                      File(document), save=True)
+                        client.active_request_id = None
+                        client.save()
+                    else:
+                        logger.error("Error downloading document: <%s, %s>" % (document, status))
+                    document.close()
+                    logger.info("Closed document.")
